2c652f8d5d21a7c532286e79759e5888157becbc
Louis Cameron Booth <me@louiscb.com>
2023-07-20 19:24:55 +0100

    First commit

--- src/main/java/com/louiscb/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/louiscb/currencyconverter/CurrencyConverter.java
@@ -10,16 +10,11 @@ public class CurrencyConverter {
     private static final double USD_TO_EUR_CONVERSION_RATE = 0.85;
 
     public static String convertCurrency(String record) {
-        // Create a Gson instance
         Gson gson = new Gson();
 
-        // Convert the JSON record to a JsonObject
         JsonObject jsonObject = gson.fromJson(record, JsonObject.class);
-
-        // Extract the price from the JsonObject
         double originalPrice = jsonObject.get("price_usd").getAsDouble();
 
-        // Convert the price to the target currency (EUR in this case)
         double convertedPrice = originalPrice * USD_TO_EUR_CONVERSION_RATE;
         BigDecimal roundedConvertedPrice = BigDecimal.valueOf(convertedPrice).setScale(2, RoundingMode.HALF_UP);
 
